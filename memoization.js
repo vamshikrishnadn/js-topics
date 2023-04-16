cd56f0b8b35c793dbb283511ba00ae4acaf0acec
vamshikrishnadn <vamshikrishnadn573@gmail.com>
2023-04-16 22:54:46 +0530

    memorization

--- memoization.js
+++ memoization.js
@@ -1,12 +1,19 @@
-function memory(n) {
-  let sum = 0;
-  for (i = 0; i <= n; i++) {
-    sum = sum + i;
+let memoryObject = {};
+
+function memoryFunction(n) {
+  if (n in memoryObject) {
+    console.log('inObj');
+    return memoryObject[n];
+  } else {
+    let sum = 0;
+    for (i = 0; i <= n; i++) {
+      sum = sum + i;
+    }
+    memoryObject.num = 10000;
+    console.log('outside', sum);
+    return sum;
   }
-
-  console.log(sum);
-
-  return sum;
 }
+console.log('🚀 ~ file:memoization.js:2 ~ memoryObject:', memoryObject);
 
-memory(20);
+memoryFunction(200);
